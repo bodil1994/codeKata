74f2c821693a86aa7482e8e92d01978858086c1b
Bodil Hundevad <bodil@hundevad.de>
2022-12-05 19:31:54 +0100

    add mapping number to array

--- Colorful Algorithm/lib/colorful.rb
+++ Colorful Algorithm/lib/colorful.rb
@@ -1,4 +1,7 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+ # split number into array
+ number.to_s.chars.map(&:to_i)
+ # add the products to the array
+ # check if there are duplicates in the array .uniq?
 end
-
