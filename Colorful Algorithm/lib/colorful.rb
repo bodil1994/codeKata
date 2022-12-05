7575254d53ebb2e2aacce9edb31d8b27471248fc
Bodil Hundevad <bodil@hundevad.de>
2022-12-05 20:12:34 +0100

    add reduce all characters

--- Colorful Algorithm/lib/colorful.rb
+++ Colorful Algorithm/lib/colorful.rb
@@ -1,7 +1,12 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
  # split number into array
- number.to_s.chars.map(&:to_i)
+ number_arr = number.to_s.chars.map(&:to_i)
+ # create variable with count of items
+ count = number_arr.count
  # add the products to the array
+ number_arr << nummber_arr.reduce(:*)
+ number_arr.each_with_index
+  number_arr <<
  # check if there are duplicates in the array .uniq?
 end
