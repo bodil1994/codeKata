323b544bad187a643e3351b32873b2fca82ece86
Bodil Hundevad <bodil@hundevad.de>
2022-12-15 00:31:27 +0100

    fix allow tag method to take optional attributes

--- Blocks_Generate HTML/lib/tag.rb
+++ Blocks_Generate HTML/lib/tag.rb
@@ -1,9 +1,6 @@
 def tag(tag_name, attributes = nil)
   # TODO: Build HTML tags around content given in the block
   #  The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
-  "<#{tag_name}>"+ yield + "</#{tag_name}>"
-end
-
-tag('h1') do
-  'Some Title'
+  name_value = attributes == nil ? nil : " #{attributes[0]}=\"#{attributes[1]}\""
+  "<#{tag_name}#{name_value}>" + yield + "</#{tag_name}>"
 end
