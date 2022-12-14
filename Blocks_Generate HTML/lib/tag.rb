6e760ec5b314b96af3d2b1d05123d33be660abcb
Bodil Hundevad <bodil@hundevad.de>
2022-12-15 00:33:37 +0100

    add test method

--- Blocks_Generate HTML/lib/tag.rb
+++ Blocks_Generate HTML/lib/tag.rb
@@ -4,3 +4,24 @@ def tag(tag_name, attributes = nil)
   name_value = attributes == nil ? nil : " #{attributes[0]}=\"#{attributes[1]}\""
   "<#{tag_name}#{name_value}>" + yield + "</#{tag_name}>"
 end
+
+# run in irb for testing
+def test
+
+  result = tag("h1") do
+    "Some Title"
+  end
+  puts result
+
+  result = tag("a", ["href", "www.google.com"]) do
+    "Google it"
+  end
+  puts result
+
+  result = tag("a", ["href", "www.google.com"]) do
+    tag("h1") do
+      "Google it"
+    end
+  end
+  puts result
+end
