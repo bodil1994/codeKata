045e5a24cb222f918992a02af8dcdddc238f5c08
Bodil Hundevad <bodil@hundevad.de>
2022-12-15 00:12:20 +0100

    fix return correct html for tag

--- Blocks_Generate HTML/lib/tag.rb
+++ Blocks_Generate HTML/lib/tag.rb
@@ -1,5 +1,9 @@
 def tag(tag_name, attributes = nil)
   # TODO: Build HTML tags around content given in the block
-  #   ush    The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
-  test
+  #  The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  "<#{tag_name}>"+ yield + "</#{tag_name}>"
+end
+
+tag('h1') do
+  'Some Title'
 end
