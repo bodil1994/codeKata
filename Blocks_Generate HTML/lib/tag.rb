e19ffaeadcacf9aac69a45c06959f54f7d48c81c
Bodil Hundevad <bodil@hundevad.de>
2022-12-15 23:48:56 +0100

    new Blocks_Splitter challenge

--- Blocks_Generate HTML/lib/tag.rb
+++ Blocks_Generate HTML/lib/tag.rb
@@ -1,6 +1,4 @@
 def tag(tag_name, attributes = nil)
-  # TODO: Build HTML tags around content given in the block
-  #  The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
   name_value = attributes == nil ? nil : " #{attributes[0]}=\"#{attributes[1]}\""
   "<#{tag_name}#{name_value}>" + yield + "</#{tag_name}>"
 end
