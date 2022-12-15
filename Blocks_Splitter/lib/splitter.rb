048db5ccd7da00b89e01fcc316511a04cc16fa70
Bodil Hundevad <bodil@hundevad.de>
2022-12-16 00:15:42 +0100

    fix size_splitter method

--- Blocks_Splitter/lib/splitter.rb
+++ Blocks_Splitter/lib/splitter.rb
@@ -2,6 +2,7 @@ def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+  array.sort.partition { |element| element.size == size }
 end
 
 def block_splitter(array)
@@ -10,3 +11,7 @@ def block_splitter(array)
   #       the second containing all the other elements.
   #       No sort needed this time.
 end
+
+words = %w(dog data ask my win two beer as)
+
+result = size_splitter(words, 3)
