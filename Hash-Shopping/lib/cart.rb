42b52310ddb4242a347a3d89c8775ff8bf67149b
Bodil Hundevad <bodil@hundevad.de>
2022-12-22 22:46:32 +0100

    solve cart, interface, store

--- Hash-Shopping/lib/cart.rb
+++ Hash-Shopping/lib/cart.rb
@@ -1,16 +1,16 @@
-# This file contains methods related to your shopping cart.
-# You should complete it according to the instructions in the TODO comments
-
 def add_to_cart(cart, product)
-  # TODO: you get a cart and a product. Add the product to the cart!
+  cart << product
 end
 
 def cart_to_s(cart)
-  # TODO: you get a cart, return a coma-separated String list of the contained products
+  cart_h = cart.group_by(&:itself).transform_values(&:count) #=  cart.group_by { |e| e } .transform_values {|e|e.count}
+  cart_h.map { |key, value| "#{key} x #{value}" }.join(", ")
 end
 
 def cart_total_price(cart, store_items)
-  # TODO: you get a cart and the store items with their prices. Return the total
-  # price of the shopping cart.
-  # /!\ Make sure it does not crash with carts having items not in store!
+  total = 0
+  cart.each do |e|
+    total += store_items[e.to_sym] unless store_items[e.to_sym].nil?
+  end
+  total
 end
--- Hash-Shopping/lib/interface.rb
+++ Hash-Shopping/lib/interface.rb
@@ -1,9 +1,8 @@
 require_relative 'store'
 require_relative 'cart'
 
-# TODO: find the correct data structure to hold the content of your shopping cart
-# instead of using nil below
-cart = nil
+# initialize cart
+cart = []
 
 puts 'Here are the available products:'
 puts store_items_to_s
@@ -19,6 +18,7 @@ loop do
 
   if product_in_store?(product)
     add_to_cart(cart, product)
+    puts "#{product} added to cart"
   else
     # otherwise show error
     puts "We don't have any #{product} in store, sorry!"
