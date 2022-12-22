cb0e97273b75915c2ac964e7dfce2b5fa8f3344a
Bodil Hundevad <bodil@hundevad.de>
2022-12-22 16:56:52 +0100

    update refactor solution

--- Hash-Anagrams/lib/group_anagrams.rb
+++ Hash-Anagrams/lib/group_anagrams.rb
@@ -1,27 +1,46 @@
 def group_anagrams(words)
-  # TODO: group anagrams
-  group_arr = []
-  words.each { |element|
-    group_arr << {
-      original: element,
-      sorted: element.chars.sort(&:casecmp).join.downcase
-    }
-  }
-  acronyms = []
-  grouped = group_arr.group_by {|hash|hash[:sorted]}
-  grouped.transform_values! do |values|
-
-    tmp = values.flat_map(&:to_a)
-     tmp = tmp.group_by(&:shift)
-    tmp.transform_values! { |values| values.flatten(1) }
-   tmp.transform_values!(&:uniq)
-   acronyms << tmp[:original]
+  groups = {} # crash_word => words
+  words.each do |word|
+    crash_word = word.downcase.chars.sort.join
+    if groups[crash_word]
+      groups[crash_word] << word
+    else
+      groups[crash_word] = [word]
+    end
   end
-  return acronyms
+  groups.values
 end
-
 
 def test
  words =  ['cars', 'for', 'POTATOES', 'racs', 'four', 'scar', 'creams', 'SCREAM']
  group_anagrams(words)
 end
+
+# def group_anagrams(words)
+#   # TODO: group anagrams
+#   grouped = create_hash_array(words)
+#   transform_hash_values(grouped)
+# end
+
+# def transform_hash_values(hash)
+#   anagrams = []
+#   hash.transform_values! do |values|
+#     tmp = values.flat_map(&:to_a)
+#     tmp = tmp.group_by(&:shift)
+#     tmp.transform_values! { |values| values.flatten(1) }
+#     tmp.transform_values!(&:uniq)
+#     anagrams << tmp[:original]
+#   end
+#   return anagrams
+# end
+
+# def create_hash_array(words)
+#   hash_array = []
+#   words.each { |element|
+#     hash_array << {
+#       original: element,
+#       sorted: element.chars.sort(&:casecmp).join.downcase
+#     }
+#   }
+#   hash_array.group_by {|hash|hash[:sorted]}
+# end
