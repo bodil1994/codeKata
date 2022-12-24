e9e4505aaa952ec46eb9034ea53132d29117dd92
Bodil Hundevad <bodil@hundevad.de>
2022-12-24 21:20:09 +0100

    new method calculate score

--- Parsing-Longest_Word/lib/longest_word.rb
+++ Parsing-Longest_Word/lib/longest_word.rb
@@ -14,6 +14,17 @@ def run_game(attempt, grid)
   call_found = call_api(attempt)
   grid_letters = (attempt.chars.uniq - grid.uniq).empty?
 
+  # initialize result hash
+  result =  {}
+
+  # word is in grid and english
+
+  # word is in grid but not english
+
+  # word is not in grid but english
+
+  # word is not in grid and not english
+
   "he given word is not an english one"
   "the given word is not in the grid"
   "the given word has the correct letters but some letters are overused"
@@ -23,5 +34,11 @@ end
 def call_api(attempt)
   url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
   call_serialized = URI.open(url).read # returns String
-  JSON.parse(call_serialized)["found"]
+  JSON.parse(call_serialized)["found"] # => returns hash - example: {"found"=>true, "word"=>"zt", "length"=>2}
+end
+
+def calculate_score(start_time, end_time, word)
+  time_ms = (end_time - start_time).round * 60
+  letters = word.grid_size
+  score = (letters * 50 * 100) / time_ms
 end
