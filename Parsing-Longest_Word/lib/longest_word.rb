bc15f11aea11358334456879efe929a86a27e5f5
Bodil Hundevad <bodil@hundevad.de>
2022-12-27 20:05:26 +0100

    new run game method

--- Parsing-Longest_Word/lib/longest_word.rb
+++ Parsing-Longest_Word/lib/longest_word.rb
@@ -8,27 +8,36 @@ def generate_grid(grid_size)
   grid
 end
 
-#def run_game(attempt, grid, start_time, end_time)
-def run_game(attempt, grid)
+def run_game(attempt, grid, start_time, end_time)
   # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
-  call_found = call_api(attempt)
-  grid_letters = (attempt.chars.uniq - grid.uniq).empty?
+  p attempt = attempt.downcase
+  p grid = grid.each(&:downcase!)
+  p call_found = call_api(attempt)
+  p grid_letters = (attempt.chars.uniq - grid.uniq).empty?
+  p grid_letters_dup = attempt.chars.uniq.size != attempt.chars.size
 
   # initialize result hash
-  result =  {}
+  result = {
+    time: end_time - start_time
+  }
 
   # word is in grid and english
-
-  # word is in grid but not english
-
-  # word is not in grid but english
-
-  # word is not in grid and not english
-
-  "he given word is not an english one"
-  "the given word is not in the grid"
-  "the given word has the correct letters but some letters are overused"
-
+  if call_found
+    if grid_letters && grid_letters_dup
+      result[:score] = calculate_score(start_time, end_time, attempt)
+      result[:message] = "the given word has the correct letters but some letters are overused"
+    elsif grid_letters
+      result[:score] = calculate_score(start_time, end_time, attempt)
+      result[:message] = "Well Done!"
+    else
+      result[:score] = 0
+      result[:message] = "the given word is not in the grid"
+    end
+  else
+    result[:score] = 0
+    result[:message] = "not an english word"
+  end
+  result
 end
 
 def call_api(attempt)
@@ -39,6 +48,7 @@ end
 
 def calculate_score(start_time, end_time, word)
   time_ms = (end_time - start_time).round * 60
-  letters = word.grid_size
+
+  letters = word.size
   score = (letters * 50 * 100) / time_ms
 end
