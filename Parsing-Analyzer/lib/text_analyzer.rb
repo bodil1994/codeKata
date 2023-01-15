a6b1f60ba8dac2b8f6ce3524ba558954a56b7be6
Bodil Hundevad <bodil@hundevad.de>
2023-01-15 15:08:47 +0100

    add character count method

--- Parsing-Analyzer/lib/text_analyzer.rb
+++ Parsing-Analyzer/lib/text_analyzer.rb
@@ -1,4 +1,15 @@
 def analyze(text)
   # TODO: should analyze the text, and return the result hash with all features
-  result = Hash.new
+  puts result = {
+    character_count: character_count(text),
+    character_count_excluding_spaces: character_count_excluding_spaces(text)
+  }
+
+end
+
+def character_count(text)
+  text.length
+end
+
+def character_count_excluding_spaces(text)
 end
