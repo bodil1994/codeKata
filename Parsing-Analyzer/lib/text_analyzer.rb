2ad2d57a7b3f757bda4330bc54b8dcc353067671
Bodil Hundevad <bodil@hundevad.de>
2023-01-15 15:28:42 +0100

    add word count method

--- Parsing-Analyzer/lib/text_analyzer.rb
+++ Parsing-Analyzer/lib/text_analyzer.rb
@@ -1,8 +1,12 @@
+#resource: https://rubular.com/
+
 def analyze(text)
   # TODO: should analyze the text, and return the result hash with all features
   puts result = {
     character_count: character_count(text),
-    character_count_excluding_spaces: character_count_excluding_spaces(text)
+    character_count_excluding_spaces: character_count_excluding_spaces(text),
+    line_count: 42,
+    word_count: word_count(text)
   }
 
 end
@@ -13,4 +17,11 @@ end
 
 def character_count_excluding_spaces(text)
   text.gsub(" ","").length
+end
+
+def line_count(text)
+end
+
+def word_count(text)
+  text.scan(/\w+/).count
 end
